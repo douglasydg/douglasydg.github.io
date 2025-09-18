8de3f35fcdbd93a5332030fa5453370acd40d37a
douglasydg <douglasydg@gmail.com>
2025-09-18 03:50:23 +0000

    Deploying to gh-pages from @ douglasydg/douglasydg.github.io@31951412484bfdef1d1bc95c5fa11b33f9a2257a 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "douglasydg/douglasyu.github.io",
+    "data-repo": "douglasydg/douglasydg.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
